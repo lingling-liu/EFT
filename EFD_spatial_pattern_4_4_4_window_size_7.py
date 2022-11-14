2c8632978ec74679dad9b3f9317bf0782c99b62c
Lingling Liu <71287710+lingling-liu@users.noreply.github.com>
2022-11-13 22:57:54 -0800

    rainbow

--- EFD_spatial_pattern_4_4_4_window_size_7.py
+++ EFD_spatial_pattern_4_4_4_window_size_7.py
@@ -5,6 +5,7 @@ https://www.kaggle.com/gauravduttakiit/satellite-imagery-analysis-using-python
 
 from asyncore import compact_traceback
 import os
+import matplotlib
 import matplotlib.pyplot as plt
 from matplotlib.colors import ListedColormap, BoundaryNorm
 import numpy as np
@@ -13,13 +14,36 @@ import rioxarray as rxr
 import earthpy as et
 #import earthpy.plot as ep
 
+cdict = {'red': ((0.0, 0.0, 0.0),
+                 (0.1, 0.5, 0.5),
+                 (0.2, 0.0, 0.0),
+                 (0.4, 0.2, 0.2),
+                 (0.6, 0.0, 0.0),
+                 (0.8, 1.0, 1.0),
+                 (1.0, 1.0, 1.0)),
+        'green':((0.0, 0.0, 0.0),
+                 (0.1, 0.0, 0.0),
+                 (0.2, 0.0, 0.0),
+                 (0.4, 1.0, 1.0),
+                 (0.6, 1.0, 1.0),
+                 (0.8, 1.0, 1.0),
+                 (1.0, 0.0, 0.0)),
+        'blue': ((0.0, 0.0, 0.0),
+                 (0.1, 0.5, 0.5),
+                 (0.2, 1.0, 1.0),
+                 (0.4, 1.0, 1.0),
+                 (0.6, 0.0, 0.0),
+                 (0.8, 0.0, 0.0),
+                 (1.0, 0.0, 0.0))}
+
+my_cmap = matplotlib.colors.LinearSegmentedColormap('my_colormap',cdict,256)
 
 # Prettier plotting with seaborn
 import seaborn as sns
 sns.set(font_scale=1.5, style="whitegrid")
 
 
-lidar_dem_path = r"C:\EFT\EFD\EFD_MODIS_EVI_2001_2017_4_4_window_size_7.tif"
+lidar_dem_path = r"C:\EFT\EFD\EFD_MODIS_CR_b4_win7.tif"
 print(lidar_dem_path)
 pre_lidar_chm1 = rxr.open_rasterio(lidar_dem_path, masked=True)
 print(type(pre_lidar_chm1))
@@ -27,7 +51,7 @@ print(pre_lidar_chm1.shape)
 #print(pre_lidar_chm1.squeeze(pre_lidar_chm1, axis=0).shape)
 #https://numpy.org/doc/stable/reference/generated/numpy.squeeze.html
 pre_lidar_chm = np.squeeze(pre_lidar_chm1, axis=0)
-pre_lidar_chm_class_ma = pre_lidar_chm.where(pre_lidar_chm !=255)
+pre_lidar_chm_class_ma = pre_lidar_chm.where(pre_lidar_chm !=0)
 print('CHM min value:', np.nanmin(pre_lidar_chm_class_ma))
 print('CHM max value:', np.nanmax(pre_lidar_chm_class_ma))
 #class_bins = list(range(1, 9, 2))
@@ -36,14 +60,14 @@ print('CHM max value:', np.nanmax(pre_lidar_chm_class_ma))
 plt.subplots(figsize=(20,4))
 
 plt.subplot(1, 4, 1)  # 1 line, 2 rows, index nr 1 (first position in the subplot)
-im = pre_lidar_chm_class_ma.plot.imshow(vmin=1, vmax=20,cmap = 'magma')
+im = pre_lidar_chm_class_ma.plot.imshow(vmin=1, vmax=20,cmap=my_cmap)
 #im = pre_lidar_chm.plot.imshow()
 #plt.colorbar(im,ticks=class_bins)
 plt.title('EFD_MODIS_b4_w7')
 plt.axis('off')
 
 
-lidar_dem_path = r"C:\EFT\EFD\EFD_Landsat7_EVI_2001_2017_4_4_window_size_7.tif"
+lidar_dem_path = r"C:\EFT\EFD\EFD_Landsat_CR_b4_win7.tif"
 print(lidar_dem_path)
 pre_lidar_chm1 = rxr.open_rasterio(lidar_dem_path, masked=True)
 print(type(pre_lidar_chm1))
@@ -51,7 +75,7 @@ print(pre_lidar_chm1.shape)
 #print(pre_lidar_chm1.squeeze(pre_lidar_chm1, axis=0).shape)
 #https://numpy.org/doc/stable/reference/generated/numpy.squeeze.html
 pre_lidar_chm = np.squeeze(pre_lidar_chm1, axis=0)
-pre_lidar_chm_class_ma = pre_lidar_chm.where(pre_lidar_chm !=255)
+pre_lidar_chm_class_ma = pre_lidar_chm.where(pre_lidar_chm !=0)
 print('CHM min value:', np.nanmin(pre_lidar_chm_class_ma))
 print('CHM max value:', np.nanmax(pre_lidar_chm_class_ma))
 
@@ -60,7 +84,7 @@ pre_lidar_chm_class_ma.plot.imshow(vmin=1, vmax=20,cmap = 'magma')
 plt.title('EFD_Landsat_b4_w7')
 plt.axis('off')
 
-lidar_dem_path = r"C:\EFT\EFD\clip\EFD_Landsat7_EVI_2001_2017_4_4_window_size_7_clp.tif"
+lidar_dem_path = r"C:\EFT\EFD\clip\EFD_Landsat_NP_clipped_b4_win7.tif"
 print(lidar_dem_path)
 pre_lidar_chm1 = rxr.open_rasterio(lidar_dem_path, masked=True)
 print(type(pre_lidar_chm1))
@@ -68,7 +92,7 @@ print(pre_lidar_chm1.shape)
 #print(pre_lidar_chm1.squeeze(pre_lidar_chm1, axis=0).shape)
 #https://numpy.org/doc/stable/reference/generated/numpy.squeeze.html
 pre_lidar_chm = np.squeeze(pre_lidar_chm1, axis=0)
-pre_lidar_chm_class_ma = pre_lidar_chm.where(pre_lidar_chm !=255)
+pre_lidar_chm_class_ma = pre_lidar_chm.where(pre_lidar_chm !=0)
 print('CHM min value:', np.nanmin(pre_lidar_chm_class_ma))
 print('CHM max value:', np.nanmax(pre_lidar_chm_class_ma))
 
@@ -78,7 +102,7 @@ plt.title('EFD_Landsat_b4_w7_NP_clip')
 plt.axis('off')
 
 
-lidar_dem_path = r"C:\EFT\EFD\EFD_Landsat7_EVI_2001_2017_NP_4_4_window_size_7.tif"
+lidar_dem_path = r"C:\EFT\EFD\EFD_Landsat_NP_b4_win7.tif"
 print(lidar_dem_path)
 pre_lidar_chm1 = rxr.open_rasterio(lidar_dem_path, masked=True)
 print(type(pre_lidar_chm1))
@@ -86,7 +110,7 @@ print(pre_lidar_chm1.shape)
 #print(pre_lidar_chm1.squeeze(pre_lidar_chm1, axis=0).shape)
 #https://numpy.org/doc/stable/reference/generated/numpy.squeeze.html
 pre_lidar_chm = np.squeeze(pre_lidar_chm1, axis=0)
-pre_lidar_chm_class_ma = pre_lidar_chm.where(pre_lidar_chm !=255)
+pre_lidar_chm_class_ma = pre_lidar_chm.where(pre_lidar_chm !=0)
 print('CHM min value:', np.nanmin(pre_lidar_chm_class_ma))
 print('CHM max value:', np.nanmax(pre_lidar_chm_class_ma))
 
